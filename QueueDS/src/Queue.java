7259fa92a544a3fd0fcb964f69ad6f86986c1670
sachin2k01 <thenamesachin@gmail.com>
2023-11-18 20:24:51 +0530

    all the operations are updated

---
+++ QueueDS/src/Queue.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Queue
+{
+    private int queue[];
+    private int size;
+    private int rear=-1;
+    private int front =0;
+    private Scanner sc=new Scanner(System.in);
+
+    public Queue(int n)
+    {
+        queue=new int[n];
+        size=queue.length;
+    }
+    public void insert()
+    {
+        int elem;
+        if(rear==size-1)
+        {
+            System.out.println("Queue is Full! Insertion is not Possible");
+        }
+        else {
+            System.out.println("Enter the element to insert");
+            elem=sc.nextInt();
+            ++rear;
+            queue[rear]=elem;
+        }
+    }
+    public void delete()
+    {
+        if(front==-1 || front>rear)
+        {
+            System.out.println("Queue is Empty");
+        }
+        else{
+            System.out.println("Deleted element is "+queue[front]);
+            ++front;
+        }
+    }
+    public void display()
+    {
+        if(front==-1 || front>rear)
+        {
+            System.out.println("queue is empty to display");
+        }
+        else{
+            for(int i : queue)
+            {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+
+        }
+    }
+}
--- QueueDS/src/QueueApp.java
+++ QueueDS/src/QueueApp.java
@@ -1,8 +1,37 @@
+import java.util.Scanner;
+
 public class QueueApp
 {
     public static void main(String[] args)
     {
         System.out.println("Welcome To Queue DataStructure");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Queue size");
+        int n=sc.nextInt();
 
+        Queue q=new Queue(n);
+        while (true)
+        {
+            System.out.println("Enter 1. for Insert");
+            System.out.println("Enter 2. for Delete");
+            System.out.println("Enter 3. for Display");
+            System.out.println("Enter any key to Exit");
+            System.out.println("Select Your Choice");
+            int ch=sc.nextInt();
+            switch (ch)
+            {
+                case 1:
+                    q.insert();
+                    break;
+                case 2:
+                    q.delete();
+                    break;
+                case 3:
+                    q.display();
+                    break;
+                default:
+                    System.exit(0);
+            }
+        }
     }
 }
